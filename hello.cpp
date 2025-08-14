67c8214b7fe355f17c1aa333c668a3840a7c74d6
Wan Ardie <wan_ahmad_ardie@yahoo.com>
2025-08-14 09:59:48 +0800

    2 window retardation

--- hello.cpp
+++ hello.cpp
@@ -7,13 +7,21 @@
 #include <child/hello.h>
 #include <child/hello2.h>
 #include <cctype>
+#include <qpushbutton.h>
+#include <qtextedit.h>
 #include <tuple>
 #include <string>
 
 #include <QApplication> // Required for the application object
+#include <QMainWindow>
 #include <QWidget>      // Required for the base window widget
 #include <QLabel>      // Required for the base window widget
 #include <QVBoxLayout>
+#include <QTextEdit>
+#include <QPushButton>
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
 
 
 
@@ -52,19 +60,44 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
   
   // Create a QWidget object. When created without a parent, it acts as a top-level window.
-  // QWidget *window = new QWidget();
-  QWidget window;
+  QWidget *window = new QWidget();
+  QMainWindow widget;
+
+  Ui::MainWindow ui;
+  ui.setupUi(&widget);
+
+  widget.show();
+  // return app.exec();
+
   // setCentralWidget(window);
 
   QVBoxLayout *layout = new QVBoxLayout(window);
 
+  
   QLabel *pointOneLongLabel = new QLabel(QApplication::translate("windowlayout", "Longitude:"));
-
   pointOneLongLabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
   pointOneLongLabel->setText("first line\nsecond line");
   pointOneLongLabel->setAlignment(Qt::AlignBottom | Qt::AlignRight);
 
+  
+  QLabel *pointTwoLongLabel = new QLabel(QApplication::translate("windowlayout", "whatever"));
+  pointTwoLongLabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+  pointTwoLongLabel->setText("my nameis peter parker");
+  pointTwoLongLabel->setAlignment(Qt::AlignBottom | Qt::AlignRight);
+
+  QTextEdit *textEdit = new QTextEdit();
+  QPushButton *pushButton = new QPushButton();
+  
+
   layout->addWidget(pointOneLongLabel);
+  layout->addWidget(pointTwoLongLabel);
+  layout->addWidget(textEdit);
+  layout->addWidget(pushButton);
+
+  QObject::connect(pushButton, &QPushButton::clicked, [textEdit]() {
+        textEdit->append("Text inserted by button.\n"); // Appends text to a new line
+        // Or textEdit->insertPlainText("Text inserted by button."); for plain text
+    });
   
   // Set the title of the window.
   window->setWindowTitle("My Simple Qt Window");
